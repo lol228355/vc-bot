04c1ac6b4237195a9637da96ca19a48cd4b4b1a8
lol228355 <149328541+lol228355@users.noreply.github.com>
2026-02-28 21:21:43 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from telebot import types
 
 # ================= НАСТРОЙКИ =================
 API_KEY_VING = 'cdlFcWjrPF7pOwJeXjhELoGYGxsVCZTrvlS396wH8QXg2XlRquw9Z7NCyi5W'
-BOT_TOKEN = '8799904851:AAF8AfaiFdvjK1fqf3BdCb0GG7JwyEVODtg'
+BOT_TOKEN = '8799904851:AAHGj1tmeeseDyavBtAw8_TREsB4UxapanQ'
 CRYPTO_TOKEN = '540404:AA2Rex1G8gtM1zNSPWa3pADmtHbWx4B2bI8'
 API_URL_VING = 'https://vingboost.ru/api/v2'
 
